6bc0f383c260fc1c66a5cab7ad6e5a5cf6dc622c
af0liveira <af0liveira@icloud.com>
2024-02-18 19:46:04 +0100

    Day 0: Hello, World.

---
+++ 00_hello_world.py
@@ -0,0 +1,28 @@
+"""Day 0: Hello, World.
+
+Task
+----
+
+To complete this challenge, you must save a line of input from stdin to a
+variable, print 'Hello, World.' on a single line, and finally print the value
+of your variable on a second line.
+
+Sample Input
+------------
+
+Welcome to 30 Days of Code!
+
+Sample Output
+-------------
+
+Hello, World. 
+Welcome to 30 Days of Code!
+"""
+
+input_string = input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print('Hello, World.')
+
+# Write the second line of output
+print(input_string)
